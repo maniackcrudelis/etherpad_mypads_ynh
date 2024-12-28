e321da1e699a400243355bf90e79882cefa42c26
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2024-12-28 16:12:23 +0100

    Bump plugin versions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -12,11 +12,11 @@ nodejs_version=20
 mypads_version=1.7.25
 
 # Plugin versions -> https://static.etherpad.org/index.html
-ep_align_version=0.3.95
+ep_align_version=0.3.97
 ep_author_hover_version=0.3.55
 ep_delete_empty_pads_version=0.0.10
-ep_headings2_version=0.2.63
-ep_font_size_version=0.4.60
+ep_headings2_version=0.2.65
+ep_font_size_version=0.4.62
 
 #=================================================
 # PERSONAL HELPERS
